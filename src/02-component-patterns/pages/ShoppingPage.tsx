5427d115efdc22453dbdae9b7d3fff530082175d
Rafael-ID <49356385+ralfId@users.noreply.github.com>
2022-09-02 10:46:21 -0600

    version 0.0.1 lista

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,7 +1,6 @@
 import { CardButtons, CardImage, CardTitle, ShoppingCard } from '../components'
 import productsArray from '../data/data';
 import { useShoppingCart } from '../hooks/useShoppingCart';
-import '../styles/custom-styles.css'
 
 export const ShoppingPage = () => {
 
@@ -20,26 +19,17 @@ export const ShoppingPage = () => {
 
                 <ShoppingCard key={product.id}
                     product={product}
-                    value={shoppingCart[product.id]?.count || 0}
-                    className="bg-dark" style={{ margin: '1em' }}
-                    onChange={onProductCountChange}
-                    initialValues={{ count: 4, maxcount: 15 }}
+                    initialValues={{ 
+                        count: 4, 
+                        //maxcount: 15
+                    }}
                 >
                     {
                         ({ reset, increasBy, count, isMaxCountReached }) => (
                             <>
-                                <CardImage className='custom-image' />
-                                <CardTitle className="text-white " />
-                                <CardButtons className="custom-btns" />
-                                <div>
-                                    <button onClick={reset}>Reset</button>
-                                    <button onClick={() => { increasBy(-2) }}>-2</button>
-                                    {
-                                        (!isMaxCountReached) &&
-                                        <button onClick={() => { increasBy(2) }}>+2</button>
-                                    }
-                                    <span style={{ color: 'white' }}>{count}</span>
-                                </div>
+                                <CardImage  />
+                                <CardTitle  />
+                                <CardButtons  />
                             </>
                         )
                     }
